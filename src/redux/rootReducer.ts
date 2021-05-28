5e5f2012e27f824360f53ff9c3af3c22ee14c40d
mjaracz <michal19996@gmail.com>
2021-05-28 11:47:01 +0200

    refactor: cleanup redux directory

--- src/redux/rootReducer.ts
+++ src/redux/rootReducer.ts
@@ -1,24 +1,8 @@
-import { changePassReducer } from 'molecules/passwordAuthentication/changePass/redux/reducers';
-import { rememberPassReducer } from 'molecules/passwordAuthentication/rememberPass/redux/reducers';
 import { signInReducer } from 'molecules/usersActivity/signIn/redux/reducers';
 import { signUpReducer } from 'molecules/usersActivity/signUp/redux/reducers';
-import {
-  deleteProductReducer,
-  listProductsReducer,
-} from 'organisms/products/redux/reducers';
-import {
-  uploadReducer,
-  userProfileReducer,
-} from 'organisms/profile/redux/reducers';
 import { combineReducers } from 'redux';
 
 export const rootReducer = combineReducers({
   signIn: signInReducer,
   signUp: signUpReducer,
-  userProfile: userProfileReducer,
-  rememberPass: rememberPassReducer,
-  changePass: changePassReducer,
-  products: listProductsReducer,
-  productDelete: deleteProductReducer,
-  upload: uploadReducer,
 });
--- src/redux/types.ts
+++ src/redux/types.ts
@@ -1,20 +1,7 @@
-import { ChangePassState } from 'molecules/passwordAuthentication/changePass/redux/types.';
-import { RememberPassState } from 'molecules/passwordAuthentication/rememberPass/redux/types';
 import { SignInState } from 'molecules/usersActivity/signIn/redux/types';
 import { SignUpState } from 'molecules/usersActivity/signUp/redux/types';
-import {
-  DeleteProductState,
-  ProductsState,
-} from 'organisms/products/redux/types';
-import { UploadState, UserProfileState } from 'organisms/profile/redux/types';
 
 export interface RootState {
   signIn: SignInState
   signUp: SignUpState
-  userProfile: UserProfileState
-  rememberPass: RememberPassState
-  changePass: ChangePassState
-  products: ProductsState
-  productDelete: DeleteProductState
-  upload: UploadState
 }
